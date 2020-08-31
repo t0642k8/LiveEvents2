c4025522da13291022d895cfdfc45c9972371ba6
James Flaherty <james.flaherty@fletc.dhs.gov>
2020-08-30 23:02:37 -0400

    Add project files.

---
+++ liveevents2/Data/Migrations/20200831025738_live1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace liveevents2.Data.Migrations
+{
+    public partial class live1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventPlace = table.Column<string>(nullable: true),
+                    EventTitle = table.Column<string>(nullable: true),
+                    EventLink = table.Column<string>(nullable: true),
+                    EventDate = table.Column<string>(nullable: true),
+                    EventSTime = table.Column<string>(nullable: true),
+                    EventETime = table.Column<string>(nullable: true),
+                    EventCenter = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
---
+++ liveevents2/Models/Events.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace liveevents2.Models
+{
+    public class Events
+    {
+        public int Id { get; set; }
+        public string EventPlace { get; set; }
+        public string EventTitle { get; set; }
+        public string EventLink { get; set; }
+        public string EventDate { get; set; }
+        public string EventSTime { get; set; }
+        public string EventETime { get; set; }
+        public string EventCenter { get; set; }
+
+
+    }
+}
